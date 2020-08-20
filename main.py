d879e1b677a16fdebf0be5616ca734d46367d16f
johnsonLT <arnoldyinglt@hotmail.com>
2020-08-20 22:46:44 +0800

    use agent class

--- main.py
+++ main.py
@@ -9,10 +9,19 @@ import sys
 import WeeklyReports
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
+
+class AgentUI(QMainWindow):
+    def __init__(self):
+        super(AgentUI, self).__init__()
+        self.initUI()
+
+    def initUI(self):
+        ui = WeeklyReports.Ui_MainWindow()
+        ui.setupUi(self)  # 向主窗口添加控件
+
+
 if __name__ == '__main__':
-    app = QApplication(sys.argv)  #应用程序
-    mainWindow = QMainWindow()  #主窗口
-    ui = WeeklyReports.Ui_MainWindow()
-    ui.setupUi(mainWindow)  #向主窗口添加控件
-    mainWindow.show()
+    app = QApplication(sys.argv)  # 应用程序
+    main = AgentUI()
+    main.show()
     sys.exit(app.exec())
